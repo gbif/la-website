c7a30c38feb59923d7548c108f7d32ae9e8e0c2d
vjrj <vjrj@ourproject.org>
2022-04-21 17:36:35 +0200

    Carousel swipe

--- assets/js/la-website.js
+++ assets/js/la-website.js
@@ -3,3 +3,24 @@
 $(function () {
   $('[data-toggle="tooltip"]').tooltip()
 })
+
+// carousel on swipe
+// https://stackoverflow.com/questions/21349984/how-to-make-bootstrap-carousel-slider-use-mobile-left-right-swipe
+$('.carousel').on('touchstart', function(event){
+  const xClick = event.originalEvent.touches[0].pageX;
+  $(this).one('touchmove', function(event){
+    const xMove = event.originalEvent.touches[0].pageX;
+    const sensitivityInPx = 5;
+
+    // https://stackoverflow.com/questions/21349984/how-to-make-bootstrap-carousel-slider-use-mobile-left-right-swipe
+    if( Math.floor(xClick - xMove) > sensitivityInPx ){
+      $(this).carousel('next');
+    }
+    else if( Math.floor(xClick - xMove) < -sensitivityInPx ){
+      $(this).carousel('prev');
+    }
+  });
+  $(this).on('touchend', function(){
+    $(this).off('touchmove');
+  });
+});

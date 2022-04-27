07a800c7cccdc968b47d5450cdeca546948028b2
vjrj <vjrj@ourproject.org>
2022-04-27 13:15:23 +0200

    Lazy load of carousel improved

--- assets/js/la-website.js
+++ assets/js/la-website.js
@@ -3,7 +3,33 @@ var rand;
 
 $(document).ready(function() {
 
+  function loadDataImg(img) {
+    // img.attr('src', img.data('lazy-load-src'));
+    img.removeAttr('loading');
+    // console.log(img.data('lazy-load-src'));
+    console.log(img.attr('src'));
+    // console.log(img);
+  }
+
+  function lazyLoadCarousel(id, next) {
+    if (next) {
+      var $nextImage = $('#la-partipants-carousel .active.carousel-item').next('.carousel-item').find(id);
+      loadDataImg($nextImage);
+    } else {
+      var $currentImage = $('#la-partipants-carousel .active.carousel-item').find(id);
+      loadDataImg($currentImage);
+    }
+  }
+
   function initCarousel(){
+
+    $('#la-partipants-carousel').on('slid.bs.carousel', function() {
+      // lazyLoadCarousel('.img-first', false);
+      // lazyLoadCarousel('.img-snd', false);
+      lazyLoadCarousel('.img-first', true);
+      lazyLoadCarousel('.img-snd', true);
+    });
+
     var $item = $('#la-partipants-carousel .carousel-inner .carousel-item');
 
     var $numberofSlides = $('#la-partipants-carousel > ol li').length;
@@ -22,10 +48,12 @@ $(document).ready(function() {
       }
     });
 
+    // Load 1st active image
+    lazyLoadCarousel('.img-first', false);
+    lazyLoadCarousel('.img-snd', false);
 
     //$('#la-partipants-carousel').css('visibility','visible');
     $('#la-partipants-carousel').fadeIn('slow');
-
 
     // Enable BS4 tooltips
     $(function () {
